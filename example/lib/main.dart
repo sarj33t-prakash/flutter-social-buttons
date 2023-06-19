c4e1b499f98b25088beaa7b6ac34861d045dbb24
Sarjeet Sandhu <psspl.trainee57@outlook.com>
2023-06-19 15:10:17 +0530

    homepage repo

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:sarj33t_login_btns/social_signin_buttons.dart';
+import 'package:sarj33t_login_btns/sarj33t_login_btns.dart';
 
 void main() {
   runApp(const MyApp());
